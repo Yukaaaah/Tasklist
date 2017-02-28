2f450e6c21075431cabeef7dea83d99dcb36cdcf
Yuka Hamano <hamano@solairo.co.jp>
2017-02-28 17:16:15 +0900

    controller, db create

--- db/migrate/20170227094651_create_tasks.rb
+++ db/migrate/20170227094651_create_tasks.rb
@@ -1,7 +1,9 @@
 class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
-
+      t.string :status,         null: false, default: "opened"
+      t.string :description
+      t.datetime :due_date
       t.timestamps null: false
     end
   end
